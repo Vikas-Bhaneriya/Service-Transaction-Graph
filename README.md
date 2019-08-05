e0537bb1df89271044bc7be3ed8fea8338c3c9a2
Vikas-Bhaneriya <vikasbhaneriy@gmail.com>
2019-08-05 16:50:42 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+Dependencies:
+1. Oracle jet toolkit should be installed and properly working in your system.
+2. npm should be installed in the system.
+
+
+Setup :
+
+clone the project using:
+
+> git clone git@github.com:Vikas-Bhaneriya/Service-Transaction-Graph.git
+
+move into the project directory and the use 
+
+> npm install 
+to install the o-jet dependencies
+build the project using 
+
+> ojet build
+> ojet serve 
+to run the application on localhost:8000
+
---
+++ src/js/viewModels/dashboard.js
@@ -0,0 +1,56 @@
+define(['knockout', 'ojs/ojbootstrap', 'ojs/ojarraydataprovider', 'text!Data/data.json',
+        'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojchart', 'ojs/ojbutton'],
+    function (ko, Bootstrap, ArrayDataProvider, quarterData) {
+
+        function ViewModel() {
+            var self = this;
+
+            self.clickedButton = ko.observable("mixedFreguency");
+            self.buttonClick = function (event) {
+                document.getElementById("lineChart2").setAttribute("time-axis-type", event.currentTarget.id + "");
+                self.clickedButton(event.currentTarget.id);
+            }.bind(self);
+
+            var currentDateTime = new Date();
+            var offset = new Date().getTimezoneOffset();
+            // to make the system clock sink with the ISO date time formate, offset is taken
+
+            var BackDateTime = new Date(currentDateTime.getTime() - 42 * 24 * 60 * 60 * 1000);
+            // the above line code will be changed
+            // moved 38 days back so that the given data can be used (25 jun 2019)
+            // we need to move 6 hours back as such
+            // if we want time to start from p hour back then put (p* 60 * 60 * 1000) in place  (39 * 24 * 60 * 60 * 1000)
+            // (39 * 24 * 60 * 60 * 1000)milliseconds will be replaced with (6 * 60 * 60 * 1000)milliseconds to move 6 hours back
+
+            var _data = JSON.parse(quarterData);
+            var modData = [];
+
+
+            for (i = 0; _data[i]; i++) {
+                modData.push({
+                    'serviceType': _data[i]['serviceType'],
+                    'count': 0,
+                    'transactionRequestDateTime': BackDateTime.toISOString()
+                });
+
+
+                for (j = 0; _data[i]['transactions'][j]; j++) {
+                    if (_data[i]['transactions'][j]['transactionRequestDateTime'] > BackDateTime.toISOString()) {
+
+                        var da = new Date(Date.parse(_data[i]['transactions'][j]['transactionRequestDateTime']) + offset * 60 * 1000);
+                        // to remove the time dependency of machine
+                        modData.push({
+                            'serviceType': _data[i]['serviceType'],
+                            'count': _data[i]['transactions'][j]['count'],
+                            'transactionRequestDateTime': da.toISOString()
+                        });
+
+                    }
+                }
+            }
+            this.dataProvider = new ArrayDataProvider(modData, {keyAttributes: 'serviceType'});
+        }
+
+        return new ViewModel();
+    }
+);
